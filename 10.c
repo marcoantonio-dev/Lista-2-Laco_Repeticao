63443a36d229ce438045f9578d0e27b47335564a
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-03-25 15:31:46 -0300

    nada

--- 10.c
+++ 10.c
@@ -26,7 +26,7 @@ void imprimirPiramide(int linhas) {
         espacos--;
 
         for (int j = 1; j <= 2 * i - 1; j++) {
-            printf("%d", j);
+            printf("%d", i);
         }
 
         printf("\n");
